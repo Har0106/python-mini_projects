608fc4465a7a4a55b8c7a83a25cca345820e5fbd
Har0106 <freddiemaron@gmail.com>
2022-06-21 19:59:41 +0530

    added random choice

--- Hungman.py
+++ Hungman.py
@@ -1,18 +1,23 @@
+import random
+
 lives = 6
-words = 'instrument'
-want = set(words)
+
+words = ['algorithm', 'argument', 'program', 'conditional', 'declaration', 'framework', 'iteration', 'autonomous', 'statements', 'compiling', 'latency', 'asynchronous', 'backpropagation', 'centroid', 'clustering', 'denoising', 'generalization', 'lambda', 'authentication', 'decryption', 'encryption', 'scareware', 'bootstrap', 'debugging', 'Deployment']
+word = random.choice(words)
+want = set(word)
 used = set()
 
 while lives != 0 and len(want) != 0:
-    l = input().lower()
+    l = input('Enter Alphabet Letter: ').lower()
     if l in want:
         want.remove(l)
         used.add(l)
     else:
         lives -= 1
-    print(''.join([i if i in used else '_' for i in words]))
+    print(''.join([i if i in used else '_' for i in word]))
 
 if len(want) == 0:
     print('You Won!')
 else:
+    print(f'The Word Was {word.capitalize()}')
     print('Sorry You Lost, Try Again.')
