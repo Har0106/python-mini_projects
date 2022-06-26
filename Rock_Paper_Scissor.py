0ae27348f7d0e0f1108e3be7c1452e364b9bdc3c
Har0106 <freddiemaron@gmail.com>
2022-06-26 19:04:48 +0530

    changed for loop to while loop

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,44 +1,41 @@
 import random
 
-def rps(n):
-    u_points = 0
-    c_points = 0
+print('Welcome to rock paper scissor game!')
+print('\nType \nR for Rock \nP for Paper \nS for Scissor\n')
+print('You will have to choose R, P or S ten times to see who wins')
 
-    for i in range(n):
-        c = random.choice(['R', 'P', 'S'])
-        u = input('R or P or S? ').capitalize()
-
-        if u != 'R' and u != 'P' and u != 'S':
-            print('Invalid Selection')
-            c_points += 1
-        elif who_wins(c,u) == 'u' or c == u:
-            u_points += 1
-        else:
-            c_points += 1
-        print(f'Points: {u_points}', f'\nRemainig Chances: {n-i-1}')
-
-    if u_points > c_points:
-        print('You Won!')
-    elif u_points == c_points:
-        print('The Game Tied!')
-    else:
-        print('Sorry, You Lost.')
-
-    print('Thank You For Playing!')
-
-def who_wins(c,u):
-    if (c == 'R' and u == 'P') or (c == 'S' and u == 'R') or (c == 'P' and u == 'S'):
-        return 'u'
-    return 'c'
-
+user_score = 0
+computer_score = 0
 n = 0
-while True:
-    n = input('How many chances do you want? ')
-    if n.isdigit() and n != '0':
-        n = int(n)
-        break
-    else:
-        print('Invalid number. Try Again.')
 
-if n != 0:
-    rps(n)
+while n < 10:
+    computer_choice = random.choice(['R', 'P', 'S'])
+    user_choice = input('R or P or S? ').capitalize()
+
+    if user_choice not in ['R', 'P', 'S']:
+        print('Invalid Selection. Try Again.')
+        continue 
+
+    if computer_choice == 'R' and user_choice == 'P':
+        print(f'You won round {n+1}')
+        user_score += 1
+    elif computer_choice == 'S' and user_choice == 'R':
+        print(f'You won round {n+1}')
+        user_score += 1   
+    elif computer_choice == 'P' and user_choice == 'S':
+        print(f'You won round {n+1}')
+        user_score += 1
+    else:
+        computer_score += 1
+        print(f'Computer won round {n+1}')
+
+    n += 1
+
+if user_score > computer_score:
+    print('You Won!')
+elif user_score == computer_score:
+    print('The Game Tied!')
+else:
+    print('Sorry, You Lost.')
+
+print('Thank You For Playing!')
