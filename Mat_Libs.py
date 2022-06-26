937c4ca3290a45b86c2007059ca4d804cffde876
Har0106 <freddiemaron@gmail.com>
2022-06-26 21:28:00 +0530

    initial commit

---
+++ Mat_Libs.py
@@ -0,0 +1,22 @@
+print('Welcome to matlib!')
+print('\nHow to cross a Piranha-infested river\n')
+
+country = input('country: ')
+adv = input('adverb: ')
+adj = input('adjective: ')
+animal = input('animal: ')
+verb = input('ing verb: ')
+verb1 = input('verb: ')
+verb2 = input('ing verb: ')
+adv1 = input('adverb: ')
+adj1 = input('adjective: ')
+place = input('place: ')
+liquid = input('type of liquid: ')
+part = input('part of the body: ')
+verb3 = input('verb: ')
+
+print(f"""\nIf you are travelling in {country} and find yourself having to cross a piranha-filled river, here is how to do it {adv}
+1. Piranha are more {adj} during the day, so cross the river at night.
+2. Avoid areas with netted {animal} traps. Piranha may be {verb} there looking to {verb1} them!
+3. When {verb2} the river swim, {adv1}. You don't want to wake them up and make them {adj1}!
+4. Whatever you do, if you have a open wound, try to find another way to get back to the {place}. Piranhas are attracted to {liquid} and will most likely take a bite out of your {part} if you {verb3} in the water!""")
