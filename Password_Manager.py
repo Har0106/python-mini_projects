cd04858eab16ea7a099e3da1044f3b3c966144d0
Har0106 <freddiemaron@gmail.com>
2022-06-27 16:23:45 +0530

    password encryption

--- Password_Manager.py
+++ Password_Manager.py
@@ -1,18 +1,29 @@
+from cryptography.fernet import Fernet
+
+# Don't use f string when encrypting or decrypting
+
 print('Welcome to password manager!')
 print('\nEnter view to view existing password.\nEnter add to add a new account and password.\nEnter quit to exit password manager.\n')
+
+# with open('key.key', 'wb') as key_file:
+#     key = Fernet.generate_key()
+#     key_file.write(key)
+
+with open('key.key', 'rb') as key_file:
+    key = key_file.read()
+
+fer = Fernet(key)
+
+def add(account, password):
+    with open('password.txt', 'a') as file:
+        file.write(f'{account}: ' + fer.encrypt(password.encode()).decode() + '\n')
 
 def view(account):
     with open('password.txt', 'r') as file:
         for i in file.readlines():
             if i.startswith(account):
-                account, password = i.strip().split(':')
-                print(f'Password: {password}')
-            else:
-                print('There is no such account.')
-
-def add(account, password):
-    with open('password.txt', 'a') as file:
-        file.write(f'{account} : {password}\n')
+                account, password = i.rstrip().split(': ')
+                print('Password:', fer.decrypt(password.encode()).decode())
 
 while True:
     activity = input('What do you want to do? ').lower()
