9e3382a6da8c21c6c5b4debd2d5815be39cdce3b
Har0106 <freddiemaron@gmail.com>
2022-06-28 22:30:39 +0530

    initial commit

--- Choose_Your_Own_Adventure.py
+++ Choose_Your_Own_Adventure.py
@@ -1,35 +1,138 @@
-print('Welcome to Exit the dangerous house!')
-print('Now you are in the 13th room')
-print('The door to exit the house can be seen in two rooms')
-print('You have to enter the number of the room\n')
+import random
 
-room =  input('Room 10/12: ')
-if room == '10':
-    print('You explored a science lab with bottled animals and picked a fire extinguisher.')
-    room = input('Room 6/8: ')
-    if room == '6':
-        print('You were let into a room with fire, but you managed to escape with the use of fire extinguisher')
-        room = input('Room 2/4: ')
+class ExitHouse():
+    def __init__(self):
+        self.lives = 0
+        self.weapon = False
+        self.qanda = [['What country has the highest life expectancy? ', 'Hong Kong'], 
+                        ['What is the most common surname in the United States? ', 'Smith'],
+                        ['Who has won the most total Academy Awards? ', 'Walt Disney'],
+                        ['How many elements are in the periodic table? ', '118'],
+                        ['Which planet in the Milky Way is the hottest? ', 'Venus']] 
+
+    def tenth_room(self):
+        print('Welome to Exit The Dangerous House!')
+        print('Now you are in room 10. Where do you want to go next?')
+
+        room = input('\nRoom 8/9: ').strip()
+        if room == '8':
+            self.eigth_room()
+        elif room == '9':
+            self.ninth_room()
+    
+    def ninth_room(self):
+        print('You entered into stone age and explored extinct animals.')
+        print('And also you recieved a weapon from caveman.')
+        self.weapon = True
+
+        room = input('\nRoom 6/7: ').strip()
+        if room == '6':
+            self.sixth_room()
+        elif room == '7':
+            self.seventh_room()
+
+    def eigth_room(self):
+        print('You entered the space and explored the galaxy.')
+        print('And also you got 2 lives from kind aliens.')
+        self. lives += 2
+
+        room = input('\nRoom 6/7: ')
+        if room == '6':
+            self.sixth_room()
+        elif room == '7':
+            self.seventh_room()
+
+    def seventh_room(self):
+        print('You entered a circus and played a lots of games.')
+        print('And also you got 5 lives from the games you won.')
+        self.lives += 5
+
+        room = input('\nRoom 2/3: ')
         if room == '2':
-            print('You were let into a room of checmical rain and died')
+            self.quiz_room()
+        elif room == '3':
+            self.third_room()
+
+    def sixth_room(self):
+        print('You entered into a palace and met the princess.')
+        print('And also you got 3 lives form her.')
+        self.lives += 3
+
+        room = input('\nRoom 4/5: ')
+        if room == '4':
+            self.fourth_room()
+        elif room == '5':
+            self.fifth_room()
+
+    def fifth_room(self):
+        print('You were burnt in the chemical rain.')
+        if self.lives >= 3:
+            print('But you survived using 4 lives')
+            self.lives -= 3
         else:
-            print('You won! You exited the dangerous house!')
-    else:
-        print('You died in the room of growing jelly moster.')
-else:
-    print('You went into a garden with flowers and butterflies.')
-    room = input('Room 9/11: ')
-    if room == '9':
+            print('Sorry. You died.')
+            return
+
+        room = input('\nRoom 0/1: ')
+        if room == '0':
+            self.win_room()
+        elif room == '1':
+            self.luck_room()
+
+    def fourth_room(self):
         print('You were eaten by a Titanoboa.')
-    else:
-        print('You went into a museum of extinct and picked 5 lives.')
-        room = input('Room 5/7: ')
-        if room == '7':
-            print('You were bitten by a Arthropleura but you used 5 lives to save yourself.')
-            room = input('Room 1/3: ')
-            if room == '1':
-                print('You won! You exited the dangerous house!')
-            else:
-                print('You died in a room full of sand')
+        if self.weapon:
+            print('You killed the Titanoboa with the weapon.')
+            self.weapon = False
         else:
-            print('You died in a room with 2000 celsius heat.')
+            print('Sorry. You died.')
+            return
+
+        room = input('\nRoom 0/3: ')
+        if room == '0':
+            self.win_room()
+        elif room == '3':
+            self.third_room()
+
+    def third_room(self):
+        print('You entered the pyramid. But it started to sink.')
+        if self.lives >= 3:
+            print('But you survived using 3 lives.')
+            self.lives -= 3
+        else:
+            print('Sorry. You died.')
+            return
+
+        room = input('\nRoom 4/5: ')
+        if room == '4':
+            self.fourth_room()
+        elif room == '5':
+            self.fifth_room()
+
+    def luck_room(self):
+        print('Welcome to the room of luck.')
+        print('We are going to toss a coin. What do you choose?')
+
+        toss = random.choice(['Head', 'Tail'])
+        choice = input('Head/Tail: ').strip().capitalize()
+        if toss == choice:
+            print('You won the game! Congratulations!')
+        else:
+            print(toss, 'on the coin.')
+            print('Sorry. You lost.')
+
+    def quiz_room(self):
+        print('Welcome to the quiz room.')
+        
+        quiz = random.choice(self.qanda)
+        answer = input(quiz[0]).strip().capitalize()
+        if answer == quiz[1]:
+            print('You won the game! Congratulations!')
+        else:
+            print('Sorry. You lost.')
+
+    def win_room(self):
+        print('You entered the with the door.')
+        print('You won the game! Congratulations!')
+    
+ExitHouse().tenth_room()
