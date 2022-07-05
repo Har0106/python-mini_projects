fe6438186ab3e68e89d53ee805b5dfac1409bde6
Har0106 <freddiemaron@gmail.com>
2022-07-05 18:17:18 +0530

    contact already exists message

--- Contact_Book.py
+++ Contact_Book.py
@@ -7,18 +7,24 @@ def add():
     connect = sqlite3.connect('contact_book.sqlite')
     cursor = connect.cursor()
     # cursor.execute('CREATE TABLE Contacts (name TEXT, phone_no TEXT, address TEXT, email TEXT)')
+    
+    name = input('Name: ').title().strip()
 
-    name = input('Name: ').title()
-    phone_no = input('Phone Number: ')
-    address = input('Address: ')
-    email = input('Email: ')
+    cursor.execute('SELECT * FROM Contacts WHERE name = ?', (name,))
+    row = cursor.fetchone()
 
-    cursor.execute('INSERT INTO Contacts (name, phone_no, address, email) VALUES (?, ?, ?, ?)',(name, phone_no, address, email))
+    if row is None:
+        phone_no = input('Phone Number: ')
+        address = input('Address: ')
+        email = input('Email: ')
+        cursor.execute('INSERT INTO Contacts (name, phone_no, address, email) VALUES (?, ?, ?, ?)',(name, phone_no, address, email))
+    else:
+        print('Contact Already Exists')
     connect.commit()
     connect.close()
 
 def view():
-    name = input('Name: ').title()
+    name = input('Name: ').title().strip()
     
     connect = sqlite3.connect('contact_book.sqlite')
     cursor = connect.cursor()
@@ -31,7 +37,7 @@ def view():
     connect.close()
 
 while True:
-    m = input('What do you want to do? ')
+    m = input('What do you want to do? ').strip().lower()
     if m == 'view':
         view()
     elif m == 'add':
